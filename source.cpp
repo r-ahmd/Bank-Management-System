72dfa6d0f2552be565c819511e25bc6c3fc84cda
tanvir_ahmed <115088617+r-ahmd@users.noreply.github.com>
2023-06-12 21:30:58 +0600

    Update source.cpp
    New Code for Bank Management with new features and benefits.

--- source.cpp
+++ source.cpp
@@ -1,101 +1,165 @@
 // ----------Bank Management System------------ //
 
-#include <iostream>
-#include <stdio.h>
-#include <conio.h>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Bank
-{
-  char name[100], adr[100], ac_t;
-  int balance;
+class BankAcc{
+  private:
+    string name;
+    int acc_num;
+    double balance;
   public:
-    void Open_account();
-    void Deposite_money();
-    void Withdraw_money();
-    void Display_account();
+    BankAcc(string n, int ac, double bl){
+      name = n;
+      acc_num = ac;
+      balance = bl;
+    }
+    string getName(){
+      return name;
+    }
+    int getAcc(){
+      return acc_num;
+    }
+    double getBal(){
+      return balance;
+    }
+    void deposite(double amount){
+        balance = balance+amount;
+    }
+    void withdraw(double amount){
+        if(balance>=amount){
+            balance -= amount;
+            cout<<"\t\tMoney Withdrawn Successfully...."<<endl;
+        }
+        else
+            cout<<"\t\tInsufficient Balance..!!!!"<<endl;
+    }
 };
-void Bank :: Open_account()
-{
-  cout<<"Enter your full name: ";
-  cin.ignore();
-  cin.getline(name, 100);
-  cout<<"Enter your address: ";
-  cin.ignore();
-  cin.getline(adr, 100);
-  cout<<"What type of account you want to open savings(s) or current(c): ";
-  cin>>ac_t;
-  cout<<"Enter amount for deposite: ";
-  cin>>balance;
-  cout<<"Your account is created";
-}
-void Bank :: Deposite_money()
-{
-  int a;
-  cout<<"Enter how much you want to Deposite: ";
-  cin>>a;
-  balance+=a;
-  cout<<"Total amount you have Deposite: \t";
-}
-void Bank :: Withdraw_money()
-{
-  float amount;
-  cout<<"\n ----Withdraw---- ";
-  cout<<"Enter amount to withdraw: ";
-  cin>>amount;
-  balance-=amount;
-  cout<<"Total amount left currently: "<<balance;
-  
-}
 
-void Bank :: Display_account()
-{
-  cout<<"Your full name: \t"<<name;
-  cout<<"Your address: \t"<<adr;
-  cout<<"Type of account that you have opened: \t"<<ac_t;
-  cout<<"Amount you have deposite : \t"<<balance;
-}
+class BankManage{
+  private:
+    vector<BankAcc> accounts;
+    public:
+        void Add_acc(string name, int acc_num, double balance){
+            accounts.push_back(BankAcc(name, acc_num, balance));
+        }
+        void Show_acc(){
+            cout<<"\t\tAll Account Holders"<<endl;
+            for(int i=0; i<accounts.size(); i++){
+                cout<<"\tName: "<<accounts[i].getName()<<endl;
+                cout<<"\tAccount Number: "<<accounts[i].getAcc()<<endl;
+                cout<<"\tBalance: "<<accounts[i].getBal()<<endl;
+            }
+        }
+        void Search_Acc(int acc){
+            cout<<"\t\tAccount Holder "<<endl;
+            for(int i=0; i<accounts.size(); i++){
+                if(accounts[i].getAcc()==acc){
+                    cout<<"\tName: "<<accounts[i].getName()<<endl;
+                    cout<<"\tAccount Number: "<<accounts[i].getAcc()<<endl;
+                    cout<<"\tBalance: "<<accounts[i].getBal()<<endl;
+                }
+            }
+        }
+        BankAcc* find_Acc(int accountNum){
+            for(int i=0; i<accounts.size(); i++){
+                if(accounts[i].getAcc()==accountNum){
+                    return &accounts[i];
+                }
+            }
+        }
+};
+
 
 int main() 
 {
-  int num, x; Bank obj;
+  BankManage Bank;
+  int choice;
+  char op;
   do{
-    cout<<"1. Open Account.   \n";
-    cout<<"2. Deposite Money. \n";
-    cout<<"3. Withdraw Money. \n";
-    cout<<"4. Display Account. \n";
-    cout<<"5. Exit System. \n";
-    cout<<"Select the option from above: \n";
-    switch(num)
-      {
-        case 1:
-          system("clear");
-          cout<<"1. Open Account.   \n";
-          obj.Open_account();
-          break;
-        case 2:
-          system("clear");
-          cout<<"2. Deposite Money. \n";
-          obj.Deposite_money();
-          break;
-        case 3:
-          system("clear");
-          cout<<"3. Withdraw Money. \n";
-          obj.Withdraw_money();
-          break;
-        case 4:
-          system("clear");
-          cout<<"4. Display Account. \n";
-          obj.Display_account();
-          break;
-        case 5: system("clear");
-          cout << "Exiting the system..." << endl;
-          exit(0);
-        default:
-          cout<<"Wrong option!!! try again \n";
-      }
-      cout << "\nDo you want to select the next option? Press 'y' to continue or 'n' to exit: ";
-      cin >> x;
-  } while (x == 'y' || x == 'Y');
-  return 0;
+      system("clear");
+      cout<<"\t\t:: Bank Management System ::"<<endl;
+      cout<<"\t\t\tMain Menu"<<endl;
+      cout<<"\t\t 1. Create New Account"<<endl;
+      cout<<"\t\t 2. Show All Account"<<endl;
+      cout<<"\t\t 3. Search Account"<<endl;
+      cout<<"\t\t 4. Deposite Money"<<endl;
+      cout<<"\t\t 5. Withdraw Money"<<endl;
+      cout<<"\t\t 6. Exit"<<endl;
+      cout<<"\t--------------------------------------"<<endl;
+      cout<<"\t\tEnter Your Choice:";
+      cin>>choice;
+      switch(choice){
+            case 1:{
+                string name;
+                int acc_num;
+                double balance;
+                cout<<"\t\tEnter Name: ";
+                cin>>name;
+                cout<<"\t\tEnter Account Number: ";
+                cin>>acc_num;
+                cout<<"\t\tEnter Initial Balance: ";
+                cin>>balance;
+                Bank.Add_acc(name, acc_num, balance);
+                cout<<"\t\tAccount Created Successfully...."<<endl;
+                break;
+            }
+            case 2:{
+                Bank.Show_acc();
+                break;
+            }
+            case 3:{
+                int account_num;
+                cout<<"\t\tEnter Account Number: ";
+                cin>>account_num;
+                Bank.Search_Acc(account_num);
+                break;
+            }
+            case 4:{
+                int accountNum;
+                double amount;
+                cout<<"\t\tEnter Account Number To Deposite Money: ";
+                cin>>accountNum;
+                BankAcc* account = Bank.find_Acc(accountNum);
+                if(account !=NULL){
+                    cout<<"\t\tEnter Amount To Deposite: ";
+                    cin>>amount;
+                    account->deposite(amount);
+                    cout<<"\t\t"<<amount<<" Deposite Successfully..."<<endl;
+                }
+                else
+                    cout<<"\t\tAccount Not Found...!!!"<<endl;
+                break;
+            }
+            case 5:{
+                int accountNum;
+                double amount;
+                cout<<"\t\tEnter Account Number To Withdraw Money: ";
+                cin>>accountNum;
+                BankAcc* account = Bank.find_Acc(accountNum);
+                if(account !=NULL){
+                    cout<<"\t\tEnter Amount To Withdraw: ";
+                    cin>>amount;
+                    account->withdraw(amount);
+                    cout<<"\t\t"<<amount<<" Withdraw Successfully..."<<endl;
+                }
+                else
+                    cout<<"\t\tAccount Not Found...!!!"<<endl;
+                break;
+            }
+            case 6:{
+                cout<<"\t\tExited From the Program";
+                exit(1);
+                break;
+            }
+            default:
+                cout<<"\t\tError!!"<<endl;
+                break;
+        }
+  cout<<"\t\tDo You Want To Continue or Exit [Yes/No]??";
+  cin>>op;
+  }while(op=='y'||op=='Y');
+  
 }
